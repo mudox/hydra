0902f85f144f062591bcf4cd84bc43cde1bf01f1
mudox <imudox@gmail.com>
2018-10-10 04:28:09 +0800

    Add `CredentialService.swift`

---
+++ Hydra/CredentialService.swift
@@ -0,0 +1,54 @@
+import SwiftyUserDefaults
+
+import GitHub
+
+import JacKit
+
+private let jack = Jack("CredentialService")
+
+class CredentialService: GitHub.CredentialServiceType {
+
+  // MARK: - Singleton
+
+  static let shared = CredentialService()
+
+  private init() {}
+
+  // MARK: - CredentialService
+
+  var app: (key: String, secret: String)? = (
+    key: "46cfca605f029f4fdb3e",
+    secret: "fba5480ff4d87ce83daf3b452da1585ddb5f5857"
+  )
+
+  var user: (name: String, password: String)? {
+    get {
+      if let username = Defaults[.username], let password = Defaults[.password] {
+        return (username, password)
+      } else {
+        jack.descendant("user").warn("no username and password")
+        return nil
+      }
+    }
+    set {
+      Defaults[.username] = newValue?.name
+      Defaults[.password] = newValue?.password
+    }
+
+  }
+
+  var token: String? {
+    get {
+      if let token = Defaults[.accessToken] {
+        return token
+      } else {
+        jack.descendant("token").warn("no access token")
+        return nil
+      }
+    }
+    set {
+      Defaults[.accessToken] = newValue
+    }
+  }
+
+}
