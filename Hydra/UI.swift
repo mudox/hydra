48a7c8b3c8039d0215997fbf171baf33c90f2d3e
mudox <imudox@gmail.com>
2018-11-23 00:28:59 +0800

    Add `UI` as UI constants namespace

---
+++ Hydra/UI.swift
@@ -0,0 +1,12 @@
+import MudoxKit
+
+// swiftlint:disable:next type_name
+enum UI {
+
+  // MARK: - Shadow
+
+  static let shadowOffset = CGSize(width: 0, height: 0)
+  static let shadowOpacity: Float = 0.8
+  static let shadowRadius: CGFloat = 2
+
+}
