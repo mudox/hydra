4cc8daa4f8d658df2d07b1cef4c3eed8c556224f
mudox <imudox@gmail.com>
2018-11-23 00:29:26 +0800

    Add `UIColor+Hydra.swift`

--- Hydra/UIColor+Hydra.swift
+++ Hydra/UIColor+Hydra.swift
@@ -4,6 +4,8 @@ import SwiftHEXColors
 
 extension UIColor {
 
+  static let hydraHighlight = UIColor(hex: 0xF5A623)!
+
   static let hydraDark = UIColor(hex: 0x4A4A4A)!
 
   static let hydraGray = UIColor(hex: 0x9B9B9B)!
