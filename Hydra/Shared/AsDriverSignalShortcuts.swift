d6459995dd191fa7887ed2ddf0892e1e7d406ade
mudox <imudox@gmail.com>
2018-12-29 00:11:52 +0800

    Improve asDriver / asSignal shortcuts

--- Hydra/Shared/AsDriverSignalShortcuts.swift
+++ Hydra/Shared/AsDriverSignalShortcuts.swift
@@ -7,29 +7,44 @@ private let jack = Jack().set(format: .short)
 
 public extension ObservableConvertibleType {
 
-  func asDriverSkipError(label: String) -> Driver<E> {
+  func asDriverSkipError(
+    label: String,
+    file: StaticString = #file,
+    function: StaticString = #function,
+    line: UInt = #line
+  ) -> Driver<E> {
     return asDriver {
-      jack.sub(label).func().failure("\(dump(of: $0))")
+      jack.sub(label).func().failure("\(dump(of: $0))", file: file, function: function, line: line)
       return .empty()
     }
   }
 
-  func asDriver(fallbackTo value: E, label: String) -> Driver<E> {
+  func asDriver(
+    fallbackTo value: E,
+    label: String, file: StaticString = #file,
+    function: StaticString = #function,
+    line: UInt = #line
+  ) -> Driver<E> {
     return asDriver {
       jack.sub(label).func().failure("""
       Captured error: \(dump(of: $0))
       Fallback to: \(value)
-      """)
+      """, file: file, function: function, line: line)
       return .just(value)
     }
   }
 
-  func asDriver(switchTo seq: Driver<E>, label: String) -> Driver<E> {
+  func asDriver(
+    switchTo seq: Driver<E>, label: String,
+    file: StaticString = #file,
+    function: StaticString = #function,
+    line: UInt = #line
+  ) -> Driver<E> {
     return asDriver {
       jack.sub(label).func().failure("""
       Captured error: \(dump(of: $0))
       Switch to: \(seq)
-      """)
+      """, file: file, function: function, line: line)
       return seq
     }
   }
@@ -37,29 +52,44 @@ public extension ObservableConvertibleType {
 
 public extension ObservableConvertibleType {
 
-  func asSignalSkipError(label: String) -> Signal<E> {
+  func asSignalSkipError(
+    label: String,
+    file: StaticString = #file,
+    function: StaticString = #function,
+    line: UInt = #line
+  ) -> Signal<E> {
     return asSignal {
-      jack.sub(label).func().failure("\(dump(of: $0))")
+      jack.sub(label).func().failure("\(dump(of: $0))", file: file, function: function, line: line)
       return .empty()
     }
   }
 
-  func asSignal(fallbackTo value: E, label: String) -> Signal<E> {
+  func asSignal(
+    fallbackTo value: E, label: String,
+    file: StaticString = #file,
+    function: StaticString = #function,
+    line: UInt = #line
+  ) -> Signal<E> {
     return asSignal {
       jack.sub(label).func().failure("""
-        Captured error: \(dump(of: $0))
-        Fallback to: \(value)
-        """)
+      Captured error: \(dump(of: $0))
+      Fallback to: \(value)
+      """, file: file, function: function, line: line)
       return .just(value)
     }
   }
 
-  func asSignal(switchTo seq: Signal<E>, label: String) -> Signal<E> {
+  func asSignal(
+    switchTo seq: Signal<E>, label: String,
+    file: StaticString = #file,
+    function: StaticString = #function,
+    line: UInt = #line
+  ) -> Signal<E> {
     return asSignal {
       jack.sub(label).func().failure("""
-        Captured error: \(dump(of: $0))
-        Switch to: \(seq)
-        """)
+      Captured error: \(dump(of: $0))
+      Switch to: \(seq)
+      """, file: file, function: function, line: line)
       return seq
     }
   }
