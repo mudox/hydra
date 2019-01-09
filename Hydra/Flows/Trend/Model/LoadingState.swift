82c198151bd5dfa663fcfcdd85e36b0c3080ce0e
mudox <imudox@gmail.com>
2019-01-09 15:46:20 +0800

    Add `LoadingState`

---
+++ Hydra/Flows/Trend/Model/LoadingState.swift
@@ -0,0 +1,80 @@
+import RxCocoa
+import RxSwift
+
+enum LoadingState<Value> {
+
+  case loading
+
+  case value(Value)
+
+  case error(Error)
+
+  var isLoading: Bool {
+    if case .loading = self {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  var value: Value! {
+    if case let .value(value) = self {
+      return value
+    } else {
+      return nil
+    }
+  }
+
+  var error: Error! {
+    if case let .error(error) = self {
+      return error
+    } else {
+      return nil
+    }
+  }
+
+  func analysis<T>(ifLoading: () -> T, ifValue: (Value) -> T, ifError: (Error) -> T) -> T {
+    switch self {
+    case .loading:
+      return ifLoading()
+    case let .value(value):
+      return ifValue(value)
+    case let .error(error):
+      return ifError(error)
+    }
+  }
+
+  func map<T>(_ transform: (Value) -> T) -> LoadingState<T> {
+    return analysis(
+      ifLoading: { .loading },
+      ifValue: { .value(transform($0)) },
+      ifError: LoadingState<T>.error
+    )
+  }
+
+  func flatMap<T>(_ transform: (Value) -> LoadingState<T>) -> LoadingState<T> {
+    return analysis(
+      ifLoading: { .loading },
+      ifValue: transform,
+      ifError: LoadingState<T>.error
+    )
+  }
+
+}
+
+extension LoadingState {
+
+}
+
+extension ObservableConvertibleType {
+
+  func asLoadingStateDriver() -> Driver<LoadingState<E>> {
+    return asObservable()
+      .map(LoadingState.value)
+      .startWith(.loading)
+      .asDriver {
+        .just(LoadingState.error($0))
+      }
+  }
+
+}
