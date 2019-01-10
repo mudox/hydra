47fff8cc4d574fbcc9bb4371c0261f5bc0108189
mudox <imudox@gmail.com>
2019-01-11 00:54:38 +0800

    Refactor `TrendBadgeView`

--- Hydra/Flows/Trend/Cell/TrendBadgeView.swift
+++ Hydra/Flows/Trend/Cell/TrendBadgeView.swift
@@ -11,27 +11,37 @@ extension TrendItemCell {
 
   class BadgeView: UIView {
 
-    private let innerDiameter: CGFloat = 18
-    private let borderWidth: CGFloat = 4
-
-    private var innerRadius: CGFloat { return innerDiameter / 2 }
-    private var outerRadius: CGFloat { return innerRadius + borderWidth }
-    private var outerDiameter: CGFloat { return outerRadius * 2 }
-
-    let rankLabel = UILabel()
-    let contentView = UIView()
-    let indicator = NVActivityIndicatorView(frame: .zero, type: .ballScaleRippleMultiple, color: .white)
-
     init() {
       super.init(frame: .zero)
 
       setupView()
     }
 
-    @available(*, unavailable)
+    @available(*, unavailable, message: "init(coder:) has not been implemented")
     required init?(coder aDecoder: NSCoder) {
-      fatalError("do not use it")
+      fatalError("init(coder:) has not been implemented")
     }
+
+    // MARK: - Metrics
+
+    private let innerDiameter: CGFloat = 18
+    private let borderWidth: CGFloat = 4
+
+    private let errorOuterWidth: CGFloat = 48
+    private var errorInnerWidth: CGFloat { return errorOuterWidth - (borderWidth * 2) }
+
+    private var innerRadius: CGFloat { return innerDiameter / 2 }
+    private var outerRadius: CGFloat { return innerRadius + borderWidth }
+    private var outerDiameter: CGFloat { return outerRadius * 2 }
+
+    // MARK: - Subviews
+
+    let rankLabel = UILabel()
+    let contentView = UIView()
+    let indicator = NVActivityIndicatorView(frame: .zero, type: .ballScaleRippleMultiple, color: .white)
+    let retryButton = UIButton(type: .custom)
+
+    // MARK: - Setup View
 
     func setupView() {
       // Self to mimic border
@@ -43,7 +53,13 @@ extension TrendItemCell {
         make.size.equalTo(outerDiameter)
       }
 
-      // Content view
+      setupContentView()
+      setupRankLabel()
+      setupIndicator()
+      setupRetryButton()
+    }
+
+    func setupContentView() {
       contentView.do {
         $0.backgroundColor = .brand
         $0.layer.cornerRadius = innerRadius
@@ -54,8 +70,9 @@ extension TrendItemCell {
         make.center.equalToSuperview()
         make.size.equalTo(innerDiameter)
       }
+    }
 
-      // Rank label
+    func setupRankLabel() {
       rankLabel.do {
         $0.text = "1"
         $0.textColor = .white
@@ -67,8 +84,9 @@ extension TrendItemCell {
       rankLabel.snp.makeConstraints { make in
         make.center.equalTo(self)
       }
+    }
 
-      // Loading indicator
+    func setupIndicator() {
       addSubview(indicator)
       indicator.snp.makeConstraints { make in
         make.center.equalToSuperview()
@@ -77,56 +95,95 @@ extension TrendItemCell {
 
     }
 
+    func setupRetryButton() {
+      retryButton.do {
+        $0.setTitle("Retry", for: .normal)
+        $0.setTitleColor(.white, for: .normal)
+        $0.titleLabel?.font = UIFont.systemFont(ofSize: 11, weight: .light)
+
+        $0.setBackgroundImage(UIImage.mdx.color(.emptyDark), for: .normal)
+        $0.setBackgroundImage(UIImage.mdx.color(.brand), for: .highlighted)
+
+        $0.layer.cornerRadius = innerRadius
+        $0.layer.masksToBounds = true
+      }
+
+      addSubview(retryButton)
+      retryButton.snp.makeConstraints { make in
+        make.size.equalTo(CGSize(width: 40, height: innerDiameter))
+        make.center.equalToSuperview()
+      }
+    }
+
+    // MARK: - Show
+
     func showLoading() {
       // Self
       isHidden = false
+      snp.updateConstraints { make in
+        make.size.equalTo(outerDiameter)
+      }
 
       // Content view
-      contentView.backgroundColor = .emptyDark
+      contentView.do {
+        $0.isHidden = false
+        $0.backgroundColor = .emptyDark
+      }
 
       // Indicator
       indicator.startAnimating()
 
-      // Rank Label
+      // Other
       rankLabel.isHidden = true
+      retryButton.isHidden = true
     }
 
     func showError() {
       // Self
       isHidden = false
+      snp.updateConstraints { make in
+        make.size.equalTo(CGSize(width: errorOuterWidth, height: outerDiameter))
+      }
 
       // Content view
-      contentView.backgroundColor = .emptyDark
+      contentView.isHidden = true
 
       // Indicator
       indicator.stopAnimating()
 
       // Rank Label
-      rankLabel.isHidden = false
-      rankLabel.text = "!"
+      rankLabel.isHidden = true
     }
 
     func show(rank: Int, color: UIColor) {
-      jack.sub("rank.didSet").assert(rank > 0, "invalid rank (\(rank)), should >= 1")
+      jack.func().assert(rank > 0, "Invalid rank (\(rank)), should >= 1")
 
-      // Hide for rank after 9
+      // Self
       if rank > 9 {
+        // Hide for rank after 9
         isHidden = true
         return
+      } else {
+        isHidden = false
       }
 
-      // Badge
-      isHidden = false
+      snp.updateConstraints { make in
+        make.size.equalTo(outerDiameter)
+      }
 
       // Content view
-      contentView.backgroundColor = color
-
-      // Indicator
-      indicator.stopAnimating()
+      contentView.do {
+        $0.isHidden = false
+        $0.backgroundColor = color
+      }
 
       // Rank label
       rankLabel.isHidden = false
       rankLabel.text = rank.description
+
+      // Other
+      indicator.stopAnimating()
+      retryButton.isHidden = true
     }
 
   }
