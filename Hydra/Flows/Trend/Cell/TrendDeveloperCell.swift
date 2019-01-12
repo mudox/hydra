e83cd6a91f7d6865ee5e4b7967edd8bf806a2352
mudox <imudox@gmail.com>
2019-01-13 02:46:53 +0800

    Add skeleton effect to `TrendDeveloperCell`

--- Hydra/Flows/Trend/Cell/TrendDeveloperCell.swift
+++ Hydra/Flows/Trend/Cell/TrendDeveloperCell.swift
@@ -3,6 +3,7 @@ import UIKit
 import RxCocoa
 import RxSwift
 
+import SkeletonView
 import SnapKit
 
 import Kingfisher
@@ -23,7 +24,9 @@ class TrendDeveloperCell: TrendCardCell {
   private var repositoryLabel: UILabel!
   private var centerStackView: UIStackView!
 
-  // MARK: - Setup
+  private var skeleton: UIStackView!
+
+  // MARK: - Setup View
 
   override init(frame: CGRect) {
     super.init(frame: frame)
@@ -37,9 +40,13 @@ class TrendDeveloperCell: TrendCardCell {
     setupRepositoryLabel()
     setupCenterStackView()
 
+    setupSkeleton()
   }
 
-  let avatarDiameter: CGFloat = 32
+  let avatarDiameter: CGFloat = 50
+  let centerInsets = UIEdgeInsets(top: 20, left: 20, bottom: 30, right: 20)
+  let gapBelowAvatarView: CGFloat = 20
+  let gapBelowNameLabel: CGFloat = 8
 
   func setupAvatarView() {
     avatarView = UIImageView().then {
@@ -94,13 +101,57 @@ class TrendDeveloperCell: TrendCardCell {
       $0.axis = .vertical
       $0.distribution = .fill
       $0.alignment = .center
-      $0.spacing = 6
+      $0.setCustomSpacing(gapBelowAvatarView, after: avatarView)
+      $0.setCustomSpacing(gapBelowNameLabel, after: nameLabel)
     }
 
     contentView.addSubview(centerStackView)
     centerStackView.snp.makeConstraints { make in
       make.center.equalToSuperview()
-      make.width.lessThanOrEqualToSuperview().inset(UIEdgeInsets.trend)
+      make.width.lessThanOrEqualToSuperview().inset(centerInsets)
+    }
+  }
+
+  func setupSkeleton() {
+    func setup(_ view: UIView, cornerRadius: CGFloat) {
+      view.isSkeletonable = true
+      view.layer.cornerRadius = cornerRadius
+      view.layer.masksToBounds = true
+    }
+
+    let avatar = UIView().then {
+      setup($0, cornerRadius: 25)
+      $0.snp.makeConstraints { make in
+        make.size.equalTo(avatarDiameter)
+      }
+    }
+
+    let nameLine = UIView().then {
+      setup($0, cornerRadius: 3)
+      $0.snp.makeConstraints { make in
+        make.size.equalTo(CGSize(width: 110, height: 14))
+      }
+    }
+
+    let repoLine = UIView().then {
+      setup($0, cornerRadius: 2)
+      $0.snp.makeConstraints { make in
+        make.size.equalTo(CGSize(width: 90, height: 11))
+      }
+    }
+
+    let views: [UIView] = [avatar, nameLine, repoLine]
+    skeleton = UIStackView(arrangedSubviews: views).then {
+      $0.isSkeletonable = true
+      $0.axis = .vertical
+      $0.distribution = .fill
+      $0.alignment = .center
+      $0.setCustomSpacing(gapBelowAvatarView, after: avatar)
+      $0.setCustomSpacing(gapBelowNameLabel, after: nameLine)
+    }
+    contentView.addSubview(skeleton)
+    skeleton.snp.makeConstraints { make in
+      make.center.equalToSuperview()
     }
   }
 
@@ -120,53 +171,28 @@ class TrendDeveloperCell: TrendCardCell {
   override func showLoading() {
     super.showLoading()
 
-    // Avatar View
-    avatarView.image = [#imageLiteral(resourceName: "Male Avatar"), #imageLiteral(resourceName: "Femail Avatar")].randomElement()!
-    avatarView.tintColor = .emptyDark
-
-    // Labels
-    nameLabel.do {
-      $0.textColor = .emptyDark
-      $0.backgroundColor = .emptyDark
-      $0.transform = .init(scaleX: 0.6, y: 0.6)
-      $0.layer.cornerRadius = 2
-      $0.layer.masksToBounds = true
-    }
-    repositoryLabel.do {
-      $0.textColor = .emptyDark
-      $0.backgroundColor = .emptyDark
-      $0.transform = .init(scaleX: 0.6, y: 0.7)
-      $0.transform = $0.transform.translatedBy(x: 0, y: -5)
-      $0.layer.cornerRadius = 2
-      $0.layer.masksToBounds = true
+    centerStackView.isHidden = true
+    skeleton.do {
+      $0.isHidden = false
+      let gradient = SkeletonGradient(baseColor: #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1))
+      let animation = SkeletonAnimationBuilder().makeSlidingAnimation(withDirection: .leftRight)
+      $0.showAnimatedGradientSkeleton(usingGradient: gradient, animation: animation)
     }
   }
 
   func show(developer: Trending.Developer, rank: Int) {
     show(rank: rank, color: .brand)
 
-    // Avatar View
+    centerStackView.isHidden = false
+    skeleton.hideSkeleton()
+
     avatarImageTask = avatarView.kf.setImage(
       with: developer.avatarURL,
       options: [.transition(.fade(0.2))]
     )
 
-    // Developer name and repository name
-    centerStackView.isHidden = false
-    nameLabel.do {
-      $0.text = developer.name
-      $0.textColor = .dark
-      $0.backgroundColor = .clear
-      $0.transform = .identity
-      $0.layer.cornerRadius = 0
-    }
-    repositoryLabel.do {
-      $0.text = developer.repositoryName
-      $0.textColor = .dark
-      $0.backgroundColor = .clear
-      $0.transform = .identity
-      $0.layer.cornerRadius = 0
-    }
+    nameLabel.text = developer.name
+    repositoryLabel.text = developer.repositoryName
   }
 
   override func show(error: Error, context: Trend.Context) {
@@ -178,6 +204,7 @@ class TrendDeveloperCell: TrendCardCell {
 
     // Center
     centerStackView.isHidden = true
+    skeleton.hideSkeleton()
   }
 
   // MARK: - Image Task
