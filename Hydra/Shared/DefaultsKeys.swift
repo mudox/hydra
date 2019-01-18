c9d9f5533e7283c435929d6bf8e500ee1c258a5e
mudox <imudox@gmail.com>
2019-01-18 09:28:49 +0800

    Remove unused defaults keys, data stored in Realm instead

--- Hydra/Shared/DefaultsKeys.swift
+++ Hydra/Shared/DefaultsKeys.swift
@@ -2,13 +2,9 @@ import SwiftyUserDefaults
 
 // MARK: - Common Prefixes
 
-private let app: String = {
-  Bundle.main.bundleIdentifier!
-}()
+private let app: String = Bundle.main.bundleIdentifier!
 
 private let credentialService = "\(app).CredentialService"
-private let launch = "\(app).Launches"
-private let languages = "\(app).Languages"
 
 extension DefaultsKeys {
 
@@ -17,18 +13,6 @@ extension DefaultsKeys {
   static let username = DefaultsKey<String?>(
     "\(credentialService).username",
     defaultValue: nil
-  )
-
-  // MARK: - Languages
-
-  static let searchedLanguages = DefaultsKey<[String]>(
-    "\(languages).searched",
-    defaultValue: []
-  )
-
-  static let pinnedLanguages = DefaultsKey<[String]>(
-    "\(languages).pinned",
-    defaultValue: Array(LanguageService.defaultPinnedLanguages)
   )
 
 }
