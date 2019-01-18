a123f5ad9fe0ed2e1833a7f61ac64fd5ebd52cbe
mudox <imudox@gmail.com>
2019-01-19 02:58:05 +0800

    Add rich comments

--- Hydra/Flows/Trend/Model/TrendModel.swift
+++ Hydra/Flows/Trend/Model/TrendModel.swift
@@ -14,11 +14,28 @@ private let jack = Jack().set(format: .short)
 // MARK: Interface
 
 protocol TrendModelInput {
+
+  /// Result from presentation of `LanguagesController`
+  ///
+  /// Flow:
+  /// 1. User tap the 'More ...' button on `LanguagesBar`.
+  /// 1. Present `LanguagesController`.
+  /// 1. User (searchs and) selects a language item.
+  /// 1. `LanguageController` is dismissed with the result.
+  var moreLanguage: BehaviorRelay<String?> { get }
+
+  /// Selected language item from language bar. Trending contents
+  /// of this language will be loaded.
   var language: BehaviorRelay<String> { get }
 }
 
 protocol TrendModelOutput {
+
+  /// Drives the outer vertical collection view.
   var trend: Driver<Trend> { get }
+
+  /// Drives `LanguagesBar.items`.
+  var barItems: Driver<[String]> { get }
 }
 
 protocol TrendModelType: TrendModelInput, TrendModelOutput {
--- Hydra/Shared/NotificationNames.swift
+++ Hydra/Shared/NotificationNames.swift
@@ -2,6 +2,7 @@ import UIKit
 
 extension Notification.Name {
 
+  /// Posted on retry button tap in trend card cell when showing error
   static let retryLoadingTrend = Notification.Name("io.github.Mudox.Hydra.Trend.retry")
 
 }
