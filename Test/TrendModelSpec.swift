3d0ac01f405cfac3269dc6e6f0285e1013fa05a0
mudox <imudox@gmail.com>
2019-01-21 02:31:45 +0800

    Add `TrendModelSpec`

---
+++ Test/TrendModelSpec.swift
@@ -0,0 +1,26 @@
+import XCTest
+
+import Nimble
+import Quick
+
+import RxBlocking
+import RxNimble
+import RxTest
+
+import JacKit
+
+@testable import Hydra
+
+private let jack = Jack("Test.Defaults")
+
+@testable import Hydra
+
+class TrendModelSpec: QuickSpec { override func spec() {
+
+  it("test") {
+    let model = TrendModel()
+    let input = model.input
+    let output = model.output
+  }
+
+} }
