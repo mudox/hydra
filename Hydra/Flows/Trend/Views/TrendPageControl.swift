1d213f0bf38808c3fdd237e05764d43cd9fc1c7b
mudox <imudox@gmail.com>
2019-01-23 05:23:31 +0800

    `TrendPageControl` shows `TrendModel.color`

--- Hydra/Flows/Trend/Views/TrendPageControl.swift
+++ Hydra/Flows/Trend/Views/TrendPageControl.swift
@@ -11,67 +11,33 @@ private let jack = Jack().set(format: .short)
 
 class TrendPageControl: UIView {
 
-  private let numberOfPages = 25
-  private let dotSize = 3
-  private let gap = 3
-
-  private var dots: [UIView]!
-
-  // MARK: - Current Index
-
-  func set(dotAt index: Int, color: UIColor, size: Int) {
-    let size = CGFloat(size)
-
-    guard dots.indices.contains(index) else {
-      return
-    }
-
-    let dot = dots[index]
-
-    dot.layer.cornerRadius = size / 2
-    dot.backgroundColor = color
-    dot.snp.updateConstraints { make in
-      make.size.equalTo(size)
-    }
-  }
-
-  func reset(dotAt index: Int) {
-    set(dotAt: index, color: .light, size: dotSize)
-  }
-
-  func lowlight(dotAt index: Int) {
-    set(dotAt: index, color: .brand, size: dotSize + 1)
-  }
-
-  func highlight(dotAt index: Int) {
-    set(dotAt: index, color: .brand, size: dotSize + 2)
-  }
-
-  var currentIndex: Int = 0 {
-    didSet {
-      currentIndex = max(0, min(24, currentIndex))
-
-      reset(dotAt: oldValue - 1)
-      reset(dotAt: oldValue)
-      reset(dotAt: oldValue + 1)
-
-      lowlight(dotAt: currentIndex - 1)
-      highlight(dotAt: currentIndex)
-      lowlight(dotAt: currentIndex + 1)
-    }
-  }
-
-  // MARK: - Setup
-
   override init(frame: CGRect) {
     super.init(frame: frame)
     setupView()
   }
 
-  @available(*, unavailable)
+  @available(*, unavailable, message: "init?(coder:) has not been implemented")
   required init?(coder aDecoder: NSCoder) {
-    fatalError("do not use it")
+    fatalError("init?(coder:) has not been implemented")
   }
+
+  // MARK: - Metrics
+
+  private let numberOfPages = 25
+  private let dotSize = 3
+  private let gap = 3
+
+  // MARK: - Colors
+
+  var majorColor: UIColor = .brand
+  var secondaryColor: UIColor = .brand
+  var normalColor: UIColor = .light
+
+  // MARK: - Subviews
+
+  private var dots: [UIView]!
+
+  // MARK: - Setup
 
   func setupView() {
     clipsToBounds = false
@@ -104,6 +70,72 @@ class TrendPageControl: UIView {
       make.height.equalTo(10)
       make.width.equalTo(dotSize * numberOfPages + gap * (numberOfPages - 1))
     }
+
+    setupBinding()
   }
 
+  // MARK: - Binding
+
+  private let bag = DisposeBag()
+
+  func setupBinding() {
+    TrendModel.color.asDriver()
+      .drive(onNext: { [weak self] color in
+        guard let self = self else { return }
+
+        self.majorColor = color
+        self.secondaryColor = color
+        self.show(index: self.currentIndex)
+      })
+      .disposed(by: bag)
+  }
+
+  // MARK: - Show Current Index
+
+  func set(dotAt index: Int, color: UIColor, size: Int) {
+    let size = CGFloat(size)
+
+    guard dots.indices.contains(index) else {
+      return
+    }
+
+    let dot = dots[index]
+
+    dot.layer.cornerRadius = size / 2
+    dot.backgroundColor = color
+    dot.snp.updateConstraints { make in
+      make.size.equalTo(size)
+    }
+  }
+
+  func reset(dotAt index: Int) {
+    set(dotAt: index, color: normalColor, size: dotSize)
+  }
+
+  func lowlight(dotAt index: Int) {
+    set(dotAt: index, color: secondaryColor, size: dotSize + 1)
+  }
+
+  func highlight(dotAt index: Int) {
+    set(dotAt: index, color: majorColor, size: dotSize + 2)
+  }
+
+  func show(index: Int) {
+    (0 ..< numberOfPages).forEach(reset)
+
+    lowlight(dotAt: index - 1)
+    highlight(dotAt: index)
+    lowlight(dotAt: index + 1)
+  }
+
+  var currentIndex: Int = 0 {
+    didSet {
+      jack.assert(
+        (0 ..< numberOfPages).contains(currentIndex),
+        "Index (\(currentIndex)) out of range \(0 ..< numberOfPages)"
+      )
+      currentIndex = max(0, min(numberOfPages - 1, currentIndex))
+      show(index: currentIndex)
+    }
+  }
 }
