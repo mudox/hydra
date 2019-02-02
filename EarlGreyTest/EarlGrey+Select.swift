eec8f67de30463121f0492bab02eb2a65c6e93f0
mudox <imudox@gmail.com>
2019-02-03 05:41:30 +0800

    Add a more versatile element(…) select function

--- EarlGreyTest/EarlGrey+Select.swift
+++ EarlGreyTest/EarlGrey+Select.swift
@@ -17,8 +17,37 @@ func element(withAID id: AID) -> GREYInteraction {
   return EarlGrey.selectElement(with: grey_accessibilityID(id.rawValue))
 }
 
-func element(withLabel label: String) -> GREYInteraction {
-  return EarlGrey.selectElement(with: grey_accessibilityLabel(label))
+func element(
+  ofType type: AnyClass? = nil,
+  withLabel label: String? = nil,
+  hasText text: String? = nil,
+  visible: Bool? = nil
+)
+  -> GREYInteraction
+{
+  var matchers = [GREYMatcher]()
+
+  if let type = type {
+    matchers.append(grey_kindOfClass(type))
+  }
+
+  if let label = label {
+    matchers.append(grey_accessibilityLabel(label))
+  }
+
+  if let text = text {
+    matchers.append(grey_text(text))
+  }
+
+  if let visible = visible {
+    if visible {
+      matchers.append(grey_sufficientlyVisible())
+    } else {
+      matchers.append(grey_notVisible())
+    }
+  }
+
+  return EarlGrey.selectElement(with: grey_allOf(matchers))
 }
 
 
