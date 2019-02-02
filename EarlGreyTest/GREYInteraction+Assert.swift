750c46f6aaa937dbadc13f0054ffb25c26d09bbb
mudox <imudox@gmail.com>
2019-02-03 04:03:15 +0800

    Add `GREYInteraction.is[Not]Visible()`

--- EarlGreyTest/GREYInteraction+Assert.swift
+++ EarlGreyTest/GREYInteraction+Assert.swift
@@ -6,6 +6,15 @@ import Quick
 import EarlGrey
 
 extension GREYInteraction {
+
+  @discardableResult
+  func isVisible() -> Self {
+    return assert(grey_sufficientlyVisible())
+  }
   
- 
+  @discardableResult
+  func isNotVisible() -> Self {
+    return assert(grey_notVisible())
+  }
+
 }
