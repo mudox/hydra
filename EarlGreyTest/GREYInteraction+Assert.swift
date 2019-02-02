23f977285188fb2f103d58eb48afd3f862b3629b
mudox <imudox@gmail.com>
2019-02-03 04:42:33 +0800

    Add more EarlGrey assert extension

--- EarlGreyTest/GREYInteraction+Assert.swift
+++ EarlGreyTest/GREYInteraction+Assert.swift
@@ -7,14 +7,37 @@ import EarlGrey
 
 extension GREYInteraction {
 
+  // MARK: - Visibility
+
   @discardableResult
   func isVisible() -> Self {
     return assert(grey_sufficientlyVisible())
   }
-  
+
   @discardableResult
   func isNotVisible() -> Self {
     return assert(grey_notVisible())
+  }
+
+  // MARK: - Interaction
+
+  @discardableResult
+  func isInteractable() -> Self {
+    return assert(grey_interactable())
+  }
+
+  @discardableResult
+  func isEnabled() -> Self {
+    return assert(grey_enabled())
+  }
+
+  /// UIButton, UILabel, UITextField, UITextView has text title or content
+  ///
+  /// - Parameter text: title or text
+  /// - Returns: GREYInteraction
+  @discardableResult
+  func has(text: String) -> Self {
+    return assert(grey_text(text))
   }
 
 }
