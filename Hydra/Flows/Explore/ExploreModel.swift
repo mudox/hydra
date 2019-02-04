bc5491940ff7d24f46669e114e5739b9bb5f3a69
mudox <imudox@gmail.com>
2019-02-04 16:48:09 +0800

    Add `ExploreModel`

---
+++ Hydra/Flows/Explore/ExploreModel.swift
@@ -0,0 +1,46 @@
+import UIKit
+
+import RxCocoa
+import RxSwift
+import RxSwiftExt
+
+import MudoxKit
+
+import JacKit
+
+private let jack = Jack().set(format: .short)
+
+// MARK: Interface
+
+protocol ExploreModelInput {
+
+}
+
+protocol ExploreModelOutput {
+
+}
+
+protocol ExploreModelType: ExploreModelInput, ExploreModelOutput {}
+
+extension ExploreModelType {
+  var input: ExploreModelInput { return self }
+  var output: ExploreModelOutput { return self }
+}
+
+// MARK: - View Model
+
+class ExploreModel: ViewModel, ExploreModelType {
+
+  // MARK: Types
+
+  // MARK: Input
+
+  // MARK: Output
+
+  // MARK: Binding
+
+  required override init() {
+
+  }
+
+}
