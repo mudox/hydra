367f7049beb602152b8b7e0de024914f374e131c
mudox <imudox@gmail.com>
2019-02-04 10:44:17 +0800

    Add `Caches.explore`

--- Hydra/Shared/Caches.swift
+++ Hydra/Shared/Caches.swift
@@ -8,7 +8,12 @@ private let jack = Jack().set(format: .short)
 
 enum Caches {
 
+  static let hour = TimeInterval(60 * 60)
+  static let day = hour * 24
+  static let week = day * 7
+
   static let languages: Storage<[GitHub.Language]>? = {
+    let expiry = Cache.Expiry.seconds(hour)
     let memoryConfig = MemoryConfig()
     let diskConfig = DiskConfig(name: "languages")
 
@@ -25,7 +30,7 @@ enum Caches {
   }()
 
   static let trend: Storage<[GitHub.Trending.Repository]>? = {
-    let expiry = Cache.Expiry.seconds(60 * 60) // expires in 1 hour
+    let expiry = Cache.Expiry.seconds(hour)
 
     let memoryConfig = MemoryConfig(expiry: expiry)
     let diskConfig = DiskConfig(name: "trend", expiry: expiry)
@@ -42,10 +47,29 @@ enum Caches {
     }
   }()
 
+  static let explore: Storage<GitHub.Explore.Lists>? = {
+    let expiry = Cache.Expiry.seconds(hour)
+
+    let memoryConfig = MemoryConfig(expiry: expiry)
+    let diskConfig = DiskConfig(name: "explore", expiry: expiry)
+
+    do {
+      return try Storage(
+        diskConfig: diskConfig,
+        memoryConfig: memoryConfig,
+        transformer: TransformerFactory.forCodable(ofType: GitHub.Explore.Lists.self)
+      )
+    } catch {
+      jack.func().error("Error initializing `Caches.explore`: \(error)")
+      return nil
+    }
+  }()
+
   static func reset() {
     do {
       try languages?.removeAll()
       try trend?.removeAll()
+      try explore?.removeAll()
     } catch {
       jack.func().warn("Error reseting caches: \(error)")
     }
