7307a5afdf36c37b42e857becec7c0c88784763c
mudox <imudox@gmail.com>
2019-02-06 11:05:18 +0800

    Add carousel to explore controller

--- Hydra/Flows/Explore/ExploreController.swift
+++ Hydra/Flows/Explore/ExploreController.swift
@@ -6,11 +6,19 @@ import RxSwift
 import JacKit
 import MudoxKit
 
+import iCarousel
+
 private let jack = Jack().set(format: .short)
 
 class ExploreController: ViewController {
 
   // MARK: - Subviews
+
+  var carousel: ExploreCarousel!
+
+  var tabView: TabView!
+
+  var collectionView: UICollectionView!
 
   // MARK: - View
 
@@ -18,10 +26,30 @@ class ExploreController: ViewController {
     view.backgroundColor = .bgDark
 
     setupNavigationBar()
+    setupCarousel()
   }
 
   func setupNavigationBar() {
     navigationController?.setNavigationBarHidden(true, animated: false)
+  }
+
+  func setupCarousel() {
+    carousel = ExploreCarousel()
+
+    view.addSubview(carousel)
+    carousel.snp.makeConstraints { make in
+      make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(8)
+      make.leading.trailing.equalToSuperview()
+      make.height.equalTo(140)
+    }
+  }
+
+  func setupTabView() {
+
+  }
+
+  func setupCollectionView() {
+
   }
 
   // MARK: - Model
@@ -29,7 +57,12 @@ class ExploreController: ViewController {
   let model: ExploreModelType = fx()
 
   override func setupModel() {
+    let output = model.output
 
+    output.carouselItems
+      .asDriver()
+      .drive(carousel.items)
+      .disposed(by: bag)
   }
 
 }
