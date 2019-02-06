4a84e75c50951b5e8d94e9f3f611050dcd36ff20
mudox <imudox@gmail.com>
2019-02-06 17:24:56 +0800

    Rename `NotificationNames.swift`

--- Hydra/Shared/NotificationNames.swift
+++
@@ -1,8 +0,0 @@
-import UIKit
-
-extension Notification.Name {
-
-  /// Posted on retry button tap in trend card cell when showing error
-  static let retryLoadingTrend = Notification.Name("io.github.Mudox.Hydra.Trend.retry")
-
-}
