a4a5f08b849a143b66059d157ca17d4cc47456f6
mudox <imudox@gmail.com>
2019-02-11 09:57:45 +0800

    Add example `ExploreEGSpec.does_not_smoke`

--- EarlGreyTest/ExploreEGSpec.swift
+++ EarlGreyTest/ExploreEGSpec.swift
@@ -4,6 +4,7 @@ import Nimble
 import Quick
 
 import GitHub
+import iCarousel
 @testable import MudoxKit
 
 @testable import Hydra
@@ -14,117 +15,40 @@ class ExploreEGSpec: QuickSpec {
 
   override func spec() {
 
-    let dismissButton = element(withAID: .dismissLanguagesBarButtonItem)
-
-    let pinBarItem = element(withAID: .pinLanguageBarButtonItem)
-    let pinButton = button(withTitle: "Pin")
-    let unpinButton = button(withTitle: "Unpin")
-
-    let backButton = button(withTitle: "Back")
-    let selectButton = button(withTitle: "Select")
-
-    let searchBar = element(withAID: .languagesSearchBar)
-
-    let loadingStateView = element(withAID: .loadingStateView)
-    let collectionView = element(withAID: .languagesCollectionView)
-
-    let swiftCellItem = element(withLabel: "Swift")
-    let unpinnedCellItem = element(withLabel: "D")
-
-    beforeEach {
+    it("does not smoke") {
+      appFlow.reset("caches")
       ExploreFlow(in: The.rootTabBarController).run.forever()
-    }
+      
+      element(withLabel: "EXPLORE").atIndex(0).tap()
+      waitCarouselToAppear()
 
-    afterEach {
-      dismissButton.tap()
-    }
-
-    it("navgation bar items initial state") {
-      backButton.isVisible()
-      pinBarItem.isNotVisible()
-    }
-
-    it("show pin when a unpinned item is selected") {
-      unpinnedCellItem
-        .using(
-          searchAction: grey_scrollInDirection(.down, 500),
-          onElementWithMatcher: grey_accessibilityID(AID.languagesCollectionView.rawValue)
-        )
-        .assert(grey_interactable())
+      button(withTitle: "Collections")
+        .isInteractable()
         .tap()
-      pinButton.isVisible()
-      selectButton.isVisible()
 
-      collectionView.scrollTo(.top)
+      button(withTitle: "Topics")
+        .isInteractable()
+        .tap()
+
+      element( .exploreContainerScrollView)
+        .fastlySwipe(.up)
+        .fastlySwipe(.down)
+        .scroll(to: .right)
+        .scroll(to: .left)
     }
-
-    it("shows unpin when a pinned item is selected") {
-      swiftCellItem.tap()
-      selectButton.isVisible()
-      unpinButton.isVisible()
-      selectedCollectionViewCell.isVisible()
-    }
-
-    it("retap the item clear selection") {
-      swiftCellItem.tap()
-      selectButton.isVisible()
-      unpinButton.isVisible()
-
-      // Re-tap
-      swiftCellItem.tap()
-      backButton.isVisible()
-      pinBarItem.isNotVisible()
-    }
-
-    it("search languages") {
-      // Scroll to unveal search bar
-      collectionView.scrollTo(.top)
-      searchBar.isVisible()
-
-      // Has match
-      searchBar.type(text: "VimScript")
-      element(ofType: UILabel.self, hasText: "VimScript", visible: true).isVisible()
-      pinBarItem.isNotVisible()
-      backButton.isVisible()
-
-      // No match
-      searchBar.clearText().type(text: "sldkjadkjf")
-      collectionView.isNotVisible()
-      loadingStateView.isVisible()
-    }
-    
-    it("clear selection before searching") {
-      swiftCellItem.tap()
-      selectedCollectionViewCell.isVisible()
-      
-      collectionView.scrollTo(.top)
-      searchBar.type(text: "Action")
-      
-      // Selection is cleared
-      selectedCollectionViewCell.isNotVisible()
-      pinBarItem.isNotVisible()
-      backButton.isVisible()
-      
-      // Select do not restore even search is cancelled
-      button(withTitle: "Cancel").tap()
-      selectedCollectionViewCell.isNotVisible()
-      pinBarItem.isNotVisible()
-      backButton.isVisible()
-    }
-
   }
 
 }
 
-private func waitCollectionViewToAppear() {
+private func waitCarouselToAppear() {
   let cond = GREYCondition(name: #function) {
     print("😈 \(#function) ...")
-    let navVC = The.rootController.presentedViewController as! UINavigationController
-    let langVC = navVC.topViewController as! LanguagesController
-    return langVC.collectionView.isHidden == false
+
+    let navVC = The.rootTabBarController.viewControllers![1] as! UINavigationController
+    let vc = navVC.viewControllers.first as! ExploreController
+    return vc.carousel.isHidden == false
   }
 
   let r = cond.wait(withTimeout: 15, pollInterval: 1)
   expect(r) == true
 }
-
